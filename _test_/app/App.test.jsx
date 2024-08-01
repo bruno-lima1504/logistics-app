5dbb3c0eab149cfa3ad2c07fdc0c6585b756944b
Bruno Lima <bruno.lima1504@gmail.com>
2024-08-01 17:59:21 -0300

    add: setup of tests

---
+++ _test_/app/App.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react-native";
+import SignIn from "../../src/pages/SingIn";
+
+it("Test exemplo", () => {
+  expect(1).toBe(1);
+});
+
+it("test render App sign component", () => {
+  render(<SignIn />);
+});
